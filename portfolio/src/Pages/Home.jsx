b5b5683c442cec52192afb05fb43a6ff71a81ad3
Davia222 <igwezedavid2@gmail.com>
2024-02-24 13:10:25 +0100

    renamed bg Image

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,56 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import img1 from "../assets/portfolio Bg Img 1/image_part_001.png"
-import img2 from "../assets/portfolio Bg Img 1/image_part_002.png"
-import img3 from "../assets/portfolio Bg Img 1/image_part_003.png"
-import img4 from "../assets/portfolio Bg Img 1/image_part_004.png"
-import img5 from "../assets/portfolio Bg Img 1/image_part_005.png"
-import img6 from "../assets/portfolio Bg Img 1/image_part_006.png"
-import img7 from "../assets/portfolio Bg Img 1/image_part_007.png"
-import img8 from "../assets/portfolio Bg Img 1/image_part_008.png"
-import img9 from "../assets/portfolio Bg Img 1/image_part_009.png"
-import img10 from "../assets/portfolio Bg Img 1/image_part_010.png"
-import img11 from "../assets/portfolio Bg Img 1/image_part_011.png"
-import img12 from "../assets/portfolio Bg Img 1/image_part_012.png"
-import img13 from "../assets/portfolio Bg Img 1/image_part_013.png"
-import img14 from "../assets/portfolio Bg Img 1/image_part_014.png"
-import img15 from "../assets/portfolio Bg Img 1/image_part_015.png"
-import img16 from "../assets/portfolio Bg Img 1/image_part_016.png"
-import img17 from "../assets/portfolio Bg Img 1/image_part_017.png"
-import img18 from "../assets/portfolio Bg Img 1/image_part_018.png"
-import img19 from "../assets/portfolio Bg Img 1/image_part_019.png"
-import img20 from "../assets/portfolio Bg Img 1/image_part_020.png"
-import img21 from "../assets/portfolio Bg Img 1/image_part_021.png"
-import img22 from "../assets/portfolio Bg Img 1/image_part_022.png"
-import img23 from "../assets/portfolio Bg Img 1/image_part_023.png"
-import img24 from "../assets/portfolio Bg Img 1/image_part_024.png"
-import img25 from "../assets/portfolio Bg Img 1/image_part_025.png"
+import img1 from "../assets/portfolioBgImg1/image_part_001.png"
+import img2 from "../assets/portfolioBgImg1/image_part_002.png"
+import img3 from "../assets/portfolioBgImg1/image_part_003.png"
+import img4 from "../assets/portfolioBgImg1/image_part_004.png"
+import img5 from "../assets/portfolioBgImg1/image_part_005.png"
+import img6 from "../assets/portfolioBgImg1/image_part_006.png"
+import img7 from "../assets/portfolioBgImg1/image_part_007.png"
+import img8 from "../assets/portfolioBgImg1/image_part_008.png"
+import img9 from "../assets/portfolioBgImg1/image_part_009.png"
+import img10 from "../assets/portfolioBgImg1/image_part_010.png"
+import img11 from "../assets/portfolioBgImg1/image_part_011.png"
+import img12 from "../assets/portfolioBgImg1/image_part_012.png"
+import img13 from "../assets/portfolioBgImg1/image_part_013.png"
+import img14 from "../assets/portfolioBgImg1/image_part_014.png"
+import img15 from "../assets/portfolioBgImg1/image_part_015.png"
+import img16 from "../assets/portfolioBgImg1/image_part_016.png"
+import img17 from "../assets/portfolioBgImg1/image_part_017.png"
+import img18 from "../assets/portfolioBgImg1/image_part_018.png"
+import img19 from "../assets/portfolioBgImg1/image_part_019.png"
+import img20 from "../assets/portfolioBgImg1/image_part_020.png"
+import img21 from "../assets/portfolioBgImg1/image_part_021.png"
+import img22 from "../assets/portfolioBgImg1/image_part_022.png"
+import img23 from "../assets/portfolioBgImg1/image_part_023.png"
+import img24 from "../assets/portfolioBgImg1/image_part_024.png"
+import img25 from "../assets/portfolioBgImg1/image_part_025.png"
 
-import img1_2 from "../assets/portfolio Bg Img 2/image_part_001.png"
-import img2_2 from "../assets/portfolio Bg Img 2/image_part_002.png"
-import img3_2 from "../assets/portfolio Bg Img 2/image_part_003.png"
-import img4_2 from "../assets/portfolio Bg Img 2/image_part_004.png"
-import img5_2 from "../assets/portfolio Bg Img 2/image_part_005.png"
-import img6_2 from "../assets/portfolio Bg Img 2/image_part_006.png"
-import img7_2 from "../assets/portfolio Bg Img 2/image_part_007.png"
-import img8_2 from "../assets/portfolio Bg Img 2/image_part_008.png"
-import img9_2 from "../assets/portfolio Bg Img 2/image_part_009.png"
-import img10_2 from "../assets/portfolio Bg Img 2/image_part_010.png"
-import img11_2 from "../assets/portfolio Bg Img 2/image_part_011.png"
-import img12_2 from "../assets/portfolio Bg Img 2/image_part_012.png"
-import img13_2 from "../assets/portfolio Bg Img 2/image_part_013.png"
-import img14_2 from "../assets/portfolio Bg Img 2/image_part_014.png"
-import img15_2 from "../assets/portfolio Bg Img 2/image_part_015.png"
-import img16_2 from "../assets/portfolio Bg Img 2/image_part_016.png"
-import img17_2 from "../assets/portfolio Bg Img 2/image_part_017.png"
-import img18_2 from "../assets/portfolio Bg Img 2/image_part_018.png"
-import img19_2 from "../assets/portfolio Bg Img 2/image_part_019.png"
-import img20_2 from "../assets/portfolio Bg Img 2/image_part_020.png"
-import img21_2 from "../assets/portfolio Bg Img 2/image_part_021.png"
-import img22_2 from "../assets/portfolio Bg Img 2/image_part_022.png"
-import img23_2 from "../assets/portfolio Bg Img 2/image_part_023.png"
-import img24_2 from "../assets/portfolio Bg Img 2/image_part_024.png"
-import img25_2 from "../assets/portfolio Bg Img 2/image_part_025.png"
+import img1_2 from "../assets/PortfolioBgImg2/image_part_001.png"
+import img2_2 from "../assets/PortfolioBgImg2/image_part_002.png"
+import img3_2 from "../assets/PortfolioBgImg2/image_part_003.png"
+import img4_2 from "../assets/PortfolioBgImg2/image_part_004.png"
+import img5_2 from "../assets/PortfolioBgImg2/image_part_005.png"
+import img6_2 from "../assets/PortfolioBgImg2/image_part_006.png"
+import img7_2 from "../assets/PortfolioBgImg2/image_part_007.png"
+import img8_2 from "../assets/PortfolioBgImg2/image_part_008.png"
+import img9_2 from "../assets/PortfolioBgImg2/image_part_009.png"
+import img10_2 from "../assets/PortfolioBgImg2/image_part_010.png"
+import img11_2 from "../assets/PortfolioBgImg2/image_part_011.png"
+import img12_2 from "../assets/PortfolioBgImg2/image_part_012.png"
+import img13_2 from "../assets/PortfolioBgImg2/image_part_013.png"
+import img14_2 from "../assets/PortfolioBgImg2/image_part_014.png"
+import img15_2 from "../assets/PortfolioBgImg2/image_part_015.png"
+import img16_2 from "../assets/PortfolioBgImg2/image_part_016.png"
+import img17_2 from "../assets/PortfolioBgImg2/image_part_017.png"
+import img18_2 from "../assets/PortfolioBgImg2/image_part_018.png"
+import img19_2 from "../assets/PortfolioBgImg2/image_part_019.png"
+import img20_2 from "../assets/PortfolioBgImg2/image_part_020.png"
+import img21_2 from "../assets/PortfolioBgImg2/image_part_021.png"
+import img22_2 from "../assets/PortfolioBgImg2/image_part_022.png"
+import img23_2 from "../assets/PortfolioBgImg2/image_part_023.png"
+import img24_2 from "../assets/PortfolioBgImg2/image_part_024.png"
+import img25_2 from "../assets/PortfolioBgImg2/image_part_025.png"
 import aboutImg from "../assets/aboutImg.png"
 
 import defautPic from "../assets/background Color icon/default.svg" 
@@ -244,6 +244,10 @@ function Home({Playfair, font, Roboto, Opensans, defaultFont, themeChanger, bgCo
 <div data-aos ="fade-right" style={font}><h1 className='text-7xl text-center worksH1'>Works</h1></div>
 
 <div data-aos ="fade-down" className="work1">
+  {/* Works1 Flexer */}
+
+  <div className='work1FlexerA' >
+
   <div className='todoListPic' style={bgColor1}><img src={todoListPic} alt="" className='todoList' /></div>
 
 
@@ -254,43 +258,67 @@ function Home({Playfair, font, Roboto, Opensans, defaultFont, themeChanger, bgCo
   </p>
   </div>
 
+  </div>
+
+<div className='work1FlexerB'>
+
+  <div className='workSpacer' ></div>
   
 <div data-aos ="fade-right" className='todoButtons'>
+
 <a href="https://davidstodoapp.vercel.app/" target='_blank' className='viewSite'>View Site</a>
 
 <a href="https://github.com/Davia222/To-do-App.git" target='_blank' className='gitHubRepo'>GitHub Repo</a>
 
 </div>
 
+</div>
+
   </div>
 
-
+{/* work 2 */}
 
 <div  className="work2" data-aos ="fade-down" >
 
-  <div className='ResturantPicDiv' style={bgColor1}><img src={figmaProjectPic} alt="" className='figmaResturant'/></div>
+<div className='work2FlexerA' >
+
+  <div className='ResturantPicDiv' style={bgColor1}><img src={figmaProjectPic} alt="" className='figmaResturant'/>
+  </div>
 
 <div className='text-center  figmaTxt'>
+  
 <h1 className='text-4xl text-center pb-3'><span>Resturant Dish Animation</span></h1>
 <p>
 Enjoy a tasty animation! Using Figma, I crafted a dynamic restaurant dish animation that seamlessly switches between breakfast and lunch with a simple click. Experience a delightful visual transition between meals!
   </p>
+
   </div>
 
-  
+  </div>
+
+ 
+  <div className='work2FlexerB' >
+
+    <div className='workSpacer'></div>
+
 <div data-aos ="fade-right" className='figButtons'>
 
-<a href="https://www.figma.com/file/0p87jLur8wqSuNi999dj4b/Resturant-dish-Breakfast-and-lunch-switch?type=design&node-id=0%3A1&mode=design&t=JgE8RF2xJ2ahEXnl-1" className='viewSite' target='_blank'>View Prototype</a>
+<a href="https://www.figma.com/file/0p87jLur8wqSuNi999dj4b/Resturant-dish-Breakfast-and-lunch-switch?type=design&node-id=0%3A1&mode=design&t=JgE8RF2xJ2ahEXnl-1" className='viewInFigmaBtn' target='_blank'>View In Figma</a>
 
   </div>
+
+  </div>
+
 </div>
 
-
-
+{/* work 3 */}
 
 <div data-aos ="fade-down" className="work3" >
 
+<div className='work3FlexerA'>
+
    <div className='smoothSkinPicDiv'  style={bgColor1} ><img src={smoothSkinPic} alt="" className='smoothSkinPic'/></div>
+   
 
 <div className='text-center smoothTxt' >
 <h1 className='text-4xl text-center pb-3' ><span>Face Smooth</span></h1>
@@ -299,12 +327,20 @@ Experience the transformation with my advanced Photoshop skills! I've expertly s
 </p>
   </div>
 
+  </div>
+
+  <div className='work3FlexerB'>
+
+<div className="work3Spacer"></div>
 
   <div data-aos ="fade-right"  className='downloadButtons'>
 
   <a href={smoothSkinPic} download className='DownloadImgBtn'>Download Image</a>
 
+  </div>
+
 </div>
+
 </div>
 
 
@@ -313,7 +349,7 @@ Experience the transformation with my advanced Photoshop skills! I've expertly s
 
 <div className='viewmoreFlexer'>
 
-<Link to="/Works" className='viewMore2' style={bgColor2} data-aos ="fade-up" ><span style={font}>View More Works</span></Link>
+<Link to="/Works" className='viewMore2'  data-aos ="fade-up" ><span style={font}>View More Works</span></Link>
 
 </div>
 
